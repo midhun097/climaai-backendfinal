8b5f0491783665d433a85b51d0cd369a6ef9dd99
midhun097 <midhun.vengalath11997@gmail.com>
2025-10-29 16:10:52 +0530

    l

---
+++ climavision_backend/api/index.py
@@ -0,0 +1,4 @@
+from myproject.wsgi import application
+
+# vercel expects a variable named 'app'
+app = application
--- climavision_backend/climavision_backend/settings.py
+++ climavision_backend/climavision_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,7 +137,7 @@ TIME_ZONE = 'Asia/Kolkata'
 USE_I18N = True
 
 USE_TZ = True
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
